3ddce15ce24406f09ed25c1854aadc183b95a3d4
Shoodaa <steven@macbookpro.speedport_w_724v_typ_a_05011603_06_003>
2025-02-02 21:43:34 +0100

    added a print to main.py for debugging

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import requests
 def get_get_btc_price():
     url = f"https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
     response = requests.get(url).json()
+    print(response)
     print(float(response["price"]))
 
 if __name__ == "__main__":
